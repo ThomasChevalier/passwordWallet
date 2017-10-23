942195f6d3ba78764c72818c5212ec5a106e1729
ThomasChevalier <thomasom.chevalier@gmail.com>
2017-10-23 13:01:20 +0200

    Little (unsuccessful) test to save option list in flash

--- Code/src/Options/Options_List.h
+++ Code/src/Options/Options_List.h
@@ -39,32 +39,48 @@ typedef struct _Option_Node
 
 #define STR(str) str##_index
 
-// #define DEFINE_NODE(name, callback, child, neighbour, strIndex) extern const __flash Option_Node name
-// #define DECLARE_NODE(name, callback, child, neighbour, strIndex) const __flash Option_Node name = {callback, child, neighbour, strIndex}
+// #define DEFINE_NODE(name, callback, child, neighbour, strIndex) extern const Option_Node name PROGMEM
+// #define DECLARE_NODE(name, callback, child, neighbour, strIndex) const Option_Node name PROGMEM = {callback, child, neighbour, strIndex}
 
-// DEFINE_NODE(n_pwd, NULL, 1, 6, 33);
-// 	DEFINE_NODE(n_pwd_change_pwd, opt_callback_change_pwd, NONE, 2, 37);
-// 	DEFINE_NODE(n_pwd_change_usr, opt_callback_change_usr_name, NONE,    3, 38);
-// 	DEFINE_NODE(n_pwd_change_nm , opt_callback_change_name,     NONE,    4, 39);
-// 	DEFINE_NODE(n_pwd_regen_pwd , opt_callback_regenerate_pwd,  NONE,    5, 40);
-// 	DEFINE_NODE(n_pwd_delete_pwd, opt_callback_delete_pwd,      NONE, NONE, 41);
+// DEFINE_NODE(n_pwd, NULL, 1, 6, STR(str_option_pwd));
+// 	DEFINE_NODE(n_change_pwd,  opt_callback_change_pwd,      NONE,    2, STR(str_option_change_pwd));
+// 	DEFINE_NODE(n_change_usr,  opt_callback_change_usr_name, NONE,    3, STR(str_option_change_usr));
+// 	DEFINE_NODE(n_change_name, opt_callback_change_name,     NONE,    4, STR(str_option_change_name));
+// 	DEFINE_NODE(n_pwd_regen,   opt_callback_regenerate_pwd,  NONE,    5, STR(str_option_regen));
+// 	DEFINE_NODE(n_pwd_del,     opt_callback_delete_pwd,      NONE, NONE, STR(str_option_delete));
 
-// DEFINE_NODE(n_sort, NULL, 7, 10, 34);
-// 	DEFINE_NODE(n_sort_no   , opt_callback_no_sort,         NONE,    8, 42);
-// 	DEFINE_NODE(n_sort_use  , opt_callback_sort_usage,      NONE,    9, 43);
-// 	DEFINE_NODE(n_sort_alpha, opt_callback_sort_alpha,      NONE, NONE, 44);
+// DEFINE_NODE(n_sort, NULL, 7, 10, STR(str_option_sort));
+// 	DEFINE_NODE(n_sort_no,    opt_callback_no_sort,         NONE,    8, STR(str_option_sort_no));
+// 	DEFINE_NODE(n_sort_use,   opt_callback_sort_use,        NONE,    9, STR(str_option_sort_use));
+// 	DEFINE_NODE(n_sort_alpha, opt_callback_sort_alpha,      NONE, NONE, STR(str_option_sort_alpha));
 
-// DEFINE_NODE(n_adv, NULL, 11, 17, 35);
-// 	DEFINE_NODE(n_adv_ch_key  , opt_callback_change_key,      NONE,   12, 45);
-// 	DEFINE_NODE(n_adv_sh_key  , opt_callback_show_key,        NONE,   13, 46);
-// 	DEFINE_NODE(n_adv_inv_sc  , opt_callback_inverse_screen,  NONE,   14, 47);
-// 	DEFINE_NODE(n_adv_keyboard, opt_callback_change_keyboard, NONE,   15, 48);
-// 	DEFINE_NODE(n_adv_self    , opt_callback_self_test,       NONE,   16, 49);
-// 	DEFINE_NODE(n_adv_reset   , opt_callback_full_reset,      NONE, NONE, 50);
+// DEFINE_NODE(n_param,NULL, 11, 21, STR(str_option_param));
 
-// DEFINE_NODE(n_add, opt_callback_add_pwd, NONE, NONE, 36);
+// 	DEFINE_NODE(n_opt_key,NULL, 12, 16, STR(str_option_key));
+// 		DEFINE_NODE(n_change_key,  opt_callback_change_key,      NONE,   13, STR(str_option_change_key));
+// 		DEFINE_NODE(n_show_key,    opt_callback_show_key,        NONE,   14, STR(str_option_show_key));
+// 		DEFINE_NODE(n_force_card,  opt_callback_force_card,      NONE,   15, STR(str_option_force_card));
+// 		DEFINE_NODE(n_force_enter, opt_callback_force_enter,     NONE,   NONE, STR(str_option_force_enter));
 
-// extern const Option_Node* const  __flash list_options [];
+// 	DEFINE_NODE(n_key_layout,  opt_callback_change_keyboard, NONE,   17, STR(str_option_keyboard_layout));
+// 	DEFINE_NODE(n_orientation, opt_callback_inverse_screen,  NONE,   18, STR(str_option_orientation));
+
+// 	DEFINE_NODE(n_self_test,  opt_callback_self_test,       NONE,   19, STR(str_option_self_test));
+// 	DEFINE_NODE(n_sys_info,   opt_callback_system_info,     NONE,   20, STR(str_system_info));
+// 	DEFINE_NODE(n_full_reset, opt_callback_full_reset,      NONE, NONE, STR(str_option_full_reset));
+
+// DEFINE_NODE(n_pwd_add, opt_callback_add_pwd, NONE, NONE, STR(str_option_add));
+
+// DEFINE_NODE(n_recovery,  NULL, NONE, 23, STR(str_recovery_title));
+// DEFINE_NODE(n_rec_full,  opt_callback_full_reset, NONE, 24, STR(str_recovery_full));
+// DEFINE_NODE(n_rec_key,   opt_callback_enter_key, NONE, 25, STR(str_recovery_key));
+// DEFINE_NODE(n_rec_card,  opt_callback_force_card, NONE, NONE, STR(str_option_force_card));
+
+// DEFINE_NODE(n_send_both, opt_callback_send_both, NONE,   27, STR(str_option_send_both));
+// DEFINE_NODE(n_send_pwd,  opt_callback_send_pwd,  NONE,   28,  STR(str_option_send_pwd));
+// DEFINE_NODE(n_send_usr,  opt_callback_send_usr,  NONE, NONE,  STR(str_option_send_usr));
+
+// extern const PGM_P const list_options [OPTIONS_LIST_NUM_OPTIONS] PROGMEM;
 
 static const Option_Node list_options [OPTIONS_LIST_NUM_OPTIONS]  = 
 {
