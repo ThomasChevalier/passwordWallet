cceecd2bb5106171f780f76f0f0bf62712fb05aa
ThomasChevalier <thomasom.chevalier@gmail.com>
2018-01-06 22:06:57 +0100

    Optimize buttons_pressed
    	modifié :         src/Hardware/Buttons.c

--- Code/src/Hardware/Buttons.c
+++ Code/src/Hardware/Buttons.c
@@ -33,20 +33,27 @@ uint8_t buttons_pressed(void)
 	// Note : there is input pullup so BUTTON_1_PIN & (1<<BUTTON_1_PIN_NUM) != 0 <=> button released
 	if (!(BUTTON_1_PIN & (1<<BUTTON_1_PIN_NUM)))
 	{
-		rep |= ( OPTIONS_FLAG & (1<<OPTIONS_FLAG_OFFSET_ORIENTATION) ) ? EVENT_BUTTON_3 : EVENT_BUTTON_1;
+		rep |= EVENT_BUTTON_1;
 	}
 	if (!(BUTTON_2_PIN & (1<<BUTTON_2_PIN_NUM)))
 	{
-		rep |= ( OPTIONS_FLAG & (1<<OPTIONS_FLAG_OFFSET_ORIENTATION) ) ? EVENT_BUTTON_4 : EVENT_BUTTON_2;
+		rep |= EVENT_BUTTON_2;
 	}
 	if (!(BUTTON_3_PIN & (1<<BUTTON_3_PIN_NUM)))
 	{
-		rep |= ( OPTIONS_FLAG & (1<<OPTIONS_FLAG_OFFSET_ORIENTATION) ) ? EVENT_BUTTON_1 : EVENT_BUTTON_3;
+		rep |= EVENT_BUTTON_3;
 	}
 	if (!(BUTTON_4_PIN & (1<<BUTTON_4_PIN_NUM)))
 	{
-		rep |= ( OPTIONS_FLAG & (1<<OPTIONS_FLAG_OFFSET_ORIENTATION) ) ? EVENT_BUTTON_2 : EVENT_BUTTON_4;
+		rep |= EVENT_BUTTON_4;
 	}
 
+	// If screen is inversed, perfom a rotation to left of 2 bit on the low nibble
+	// /!\ EVENT_BUTTON_1 to EVENT_BUTTON_4 must be in range 0x01 to 0x0F
+	if(OPTIONS_FLAG & (1<<OPTIONS_FLAG_OFFSET_ORIENTATION))
+	{
+		rep <<= 2;
+		rep = (rep & 0x0F) | (rep >> 4);
+	}
 	return rep;
 }
