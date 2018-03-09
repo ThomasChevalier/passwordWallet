a6ce61bdfb9047036e69e9b6305133d05c9b064b
ThomasChevalier <thomasom.chevalier@gmail.com>
2018-03-09 19:31:49 +0100

    Improve random generator of memory editor

--- Utility/memory_editor/src/Random.cpp
+++ Utility/memory_editor/src/Random.cpp
@@ -1,11 +1,18 @@
 #include "Random.h"
 
+#include <random>
+#include <stdexcept>
+
 uint8_t random_request_byte (void)
 {
-	return 1;
+	std::random_device rd("/dev/urandom");
+	std::uniform_int_distribution<int> distribution(0,255);
+	return distribution(rd);
 }
 
 uint8_t random_request_printable (void)
 {
-	return 'a';
+	std::random_device rd("/dev/urandom");
+	std::uniform_int_distribution<int> distribution(' ','~'-1);
+	return distribution(rd);
 }
